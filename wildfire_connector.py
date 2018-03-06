3c5c5e5959bcdd1e2c3c3d751227237e1f59fac6
Alexa Lomotan <alexa.lomotan@phantom.us>
2018-03-06 14:33:08 -0800

    PR fixes

--- wildfire_connector.py
+++ wildfire_connector.py
@@ -537,28 +537,29 @@ class WildfireConnector(BaseConnector):
             return action_result.get_status()
 
         # get verdict whether hash is in WildFire database
-        verdict_code = int(response['get-verdict-info']['verdict'])
-
         try:
-            if verdict_code == 0:
-                verdict = {0: 'benign'}
-            elif verdict_code == 1:
-                verdict = {1: 'malware'}
-            elif verdict_code == 2:
-                verdict = {2: 'grayware'}
-            elif verdict_code == 4:
-                verdict = {4: 'phishing'}
-            elif verdict_code == -100:
-                verdict = {-100: 'pending, the sample exists, but there is currently no verdict'}
-            elif verdict_code == -101:
-                verdict = {-101: 'error'}
-            elif verdict_code == -102:
-                verdict = {-102: 'unknown, cannot find sample record in the WildFire database'}
-            elif verdict_code == -103:
-                verdict = {-103: 'invalid hash value'}
-            return verdict
+            verdict_code = int(response['get-verdict-info']['verdict'])
         except:
-            return "verdict unknown"
+            return "verdict cannot be retrieved"
+
+        if verdict_code == 0:
+            verdict = {0: 'benign'}
+        elif verdict_code == 1:
+            verdict = {1: 'malware'}
+        elif verdict_code == 2:
+            verdict = {2: 'grayware'}
+        elif verdict_code == 4:
+            verdict = {4: 'phishing'}
+        elif verdict_code == -100:
+            verdict = {-100: 'pending, the sample exists, but there is currently no verdict'}
+        elif verdict_code == -101:
+            verdict = {-101: 'error'}
+        elif verdict_code == -102:
+            verdict = {-102: 'unknown, cannot find sample record in the WildFire database'}
+        elif verdict_code == -103:
+            verdict = {-103: 'invalid hash value'}
+
+        return verdict
 
     def _detonate_url(self, param):
 
@@ -587,7 +588,6 @@ class WildfireConnector(BaseConnector):
             return action_result.set_status(phantom.APP_ERROR, "task id not part of response, can't continue")
 
         verdict = self._get_verdict(task_id, action_result)
-
         try:
             verdict_code, verdict_message = verdict.items()[0]
         except:
