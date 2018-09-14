1fc99429985e5de294a80168898729cbf13caec5
michellel <michellel@splunk.com>
2018-09-13 23:13:18 -0700

    updated copyright to Splunk

--- wildfire_consts.py
+++ wildfire_consts.py
@@ -1,16 +1,8 @@
-# --
-# File: wildfire_consts.py
+# Copyright (c) 2014-2018 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2018
+# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 #
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
 
 WILDFIRE_JSON_BASE_URL = "base_url"
 WILDFIRE_JSON_TASK_ID = "task_id"
