3f90d4506391a21075ef8cd4da3981093b8ab122
michellel <michellel@splunk.com>
2018-09-14 10:21:21 -0700

    corrected the copyright year

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2018 Splunk Inc.
+# Copyright (c) 2016-2018 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
