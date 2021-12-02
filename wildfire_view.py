80d852b52991f080f0cf6c50cd2f17ec6bcb6945
splunk-soar-connectors-admin <admin@splunksoar>
2021-12-02 09:39:55 -0800

    Updating source files with legal boilerplate

--- wildfire_view.py
+++ wildfire_view.py
@@ -1,11 +1,17 @@
 # File: wildfire_view.py
+#
 # Copyright (c) 2016-2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-
-
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
 def json_dump_view(provides, all_app_runs, context):
 
     # context['jsons'] = [json.dumps(item, separators=(',', ':'), sort_keys=True, indent=4) for item in j]
